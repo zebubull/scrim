efb0c749c35b19d2caeeb023055037ca3b252220
zebubull <ldkourtev@gmail.com>
2023-11-18 18:16:25 -0500

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 cargo install --path .
 
-mkdir ~/.scrim
+mkdir -p ~/.scrim
 cp lookups/spells.json ~/.scrim/spells.json
+cp lookups/weapons.json ~/.scrim/weapons.json
+cp lookups/classes.json ~/.scrim/classes.json
+cp lookups/races.json ~/.scrim/races.json
+cp lookups/subclasses.json ~/.scrim/subclasses.json
 
 echo "installed successfully"
