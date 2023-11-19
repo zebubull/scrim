54fa72c4c6999c90f1ca3d97b6309bf09f25b422
zebubull <ldkourtev@gmail.com>
2023-11-19 15:25:00 -0500

    thingy

--- src/update.rs
+++ src/update.rs
@@ -43,8 +43,10 @@ pub fn update(app: &mut App, lookup: &Lookup, key_event: KeyEvent) -> Result<()>
                 // Stats and may have changed so we need to do this. It might
                 // be better to change stat and level to CycleFn but i'm too lazy
                 // to check if that's faster and the calculations are relatively light
-                app.player.update_stat_dependants();
-                app.player.update_level_dependants();
+                if let Some(Selected::InfoItem(_)) = app.selected {} else {
+                    app.player.update_stat_dependants();
+                    app.player.update_level_dependants();
+                }
             }
             Some(ControlType::CycleFn(prev, next)) => {
                 match key_event.code {
